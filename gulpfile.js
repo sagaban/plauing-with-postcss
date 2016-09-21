b45284d37cc86cba0a5dec52a07e5c7716101983
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2016-09-21 18:24:53 +0200

    Add source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
     return gulp.src('src/*.css')
 		.pipe(postcss([ autoprefixer ]))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('maps/'))
 		.pipe(gulp.dest('dest/'));
 });
