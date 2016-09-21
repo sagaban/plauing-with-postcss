bd8a3d43203479ba6dc23cf00838c0b64d6fe1a1
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2016-09-21 18:32:59 +0200

    Minify and rename the CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,24 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var cssnano = require('cssnano');
 
 gulp.task('styles', function() {
     return gulp.src('src/*.css')
-		.pipe(postcss([ autoprefixer ]))
-		.pipe(sourcemaps.init())
-		.pipe(sourcemaps.write('maps/'))
-		.pipe(gulp.dest('dest/'));
+        .pipe(postcss([autoprefixer]))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('maps/'))
+        .pipe(gulp.dest('dest/'));
 });
+
+gulp.task('rename', ['styles'], function() {
+    return gulp.src('dest/example.css')
+        .pipe(postcss([cssnano]))
+        .pipe(rename('example.min.css'))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('maps/'))
+        .pipe(gulp.dest("dest/"));
+});
+
+gulp.task('default', ['styles', 'rename']);
